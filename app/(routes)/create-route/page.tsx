1f871426570a9724c7e007cff5b3ed3403bafd36
ecaleb97 <valley_rob.0c@icloud.com>
2024-03-16 03:22:11 +0100

    Add search functionality and display map image and mini cards on create route page

--- app/(routes)/create-route/page.tsx
+++ app/(routes)/create-route/page.tsx
@@ -1,14 +1,32 @@
 import { Footer } from "@/components/footer/footer"
 import { raleway } from "@/components/ui/fonts"
+import Image from "next/image"
+import map2 from "@/app/assets/images/map2.png"
+import { Search } from "@/components/search/search"
+import { MiniCard } from "@/components/card/mini-card"
 
 export default function MyRoute () {
   return (
     <div className={`my-6 ${raleway.className} font-semibold text-[#424B54]`}>
       <h3 
-        className="text-center"
+        className="text-center mb-6"
       >
         Crea tu ruta personalizada
+        
       </h3>
+      <Search />
+      <Image 
+        src={map2} 
+        alt="Map" 
+        width={500} 
+        height={250} 
+        priority 
+        className="my-6" 
+      />
+      <div className="flex justify-around mb-20">
+        <MiniCard message="Rutas anteriores" />
+        <MiniCard message="Rutas guardadas" />
+      </div>
       <Footer />
     </div>
   )
