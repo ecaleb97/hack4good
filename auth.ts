f0768da512270c3680d2fe847e74af094da895b1
ecaleb97 <valley_rob.0c@icloud.com>
2024-03-16 13:40:44 +0100

    Remove unused import in auth.ts

--- auth.ts
+++ auth.ts
@@ -3,7 +3,6 @@ import authConfig from "@/auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "@/lib/db"
 import { getUserById } from "@/data/user"
-import { UserRole } from "@prisma/client"
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation"
 import { getAccountByUserId } from "@/data/account"
 
